bb23bdf309b5118e91a56d49a5c11edb0e8e6552
madgustoo <agha97a@gmail.com>
2025-11-06 22:26:33 -0500

    New player states handled

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,20 +4,19 @@ extends CharacterBody2D
 @onready var ray_cast_down: RayCast2D = $RayCast2D
 @onready var coyote_timer: Timer = $CoyoteTimer
 @onready var camera: Camera2D = $Camera2D
+@onready var collision_shape: CollisionShape2D = $CollisionShape2D
 
 @export_range(0, 1) var acceleration = 1
 @export_range(0, 1) var deceleration = 1
 @export_range(0, 1) var decelerate_on_jump_release = 0.5
 
 enum State {
-	NORMAL, # IDLE
-	SLEEP,
-	RUN,
+	NORMAL,
 	JUMPING,
 	FALL,
 	SNEAK,
-	WALL_SLIDE,
-	WALL_CLIMB
+	WALL_CLIMB,
+	WALL_SLIDE
 }
 
 var state = State.NORMAL
@@ -27,47 +26,22 @@ const MAX_FALL_SPEED = 600.0
 const JUMP_VELOCITY = -320.0
 const WALL_JUMP_VELOCITY = -250.0
 const FALL_GRAVITY = 1000
-const SNEAK_SPEED: float = 60.0
-const JUMP_BUFFER_TIME: float = 0.1
+const SNEAK_SPEED = 60.0
+const WALL_CLIMB_SPEED = 100.0
+const WALL_SLIDE_SPEED = 80.0
+const WALL_GRAVITY = 200.0
+const WALL_JUMP_PUSH_FORCE = 120.0
 
-const WALL_CLIMB_SPEED: float = 80.0
-const WALL_SLIDE_SPEED: float = 60
-const WALL_GRAVITY: float = 200.0
-const WALL_JUMP_PUSH_FORCE: float = 120.0
+var just_landed = false
 
-var is_sneaking = false
-var just_landed = false # Used to play the land animation
-
-# Looking right by default
-var look_dir_x: int = 1
-
-# This is so the player has a little more time after leaving a wall to still perform a wall jump
-# The amount of time we still consider the player on the wall
-var wall_contact_coyote: float = 0.0
-# The amount of time we provide to wall_contact_coyote
-const WALL_CONTACT_COYOTE_TIME: float = 0.2
-
-# This is to to allow the push force from the wall to work
-# The amount of time left that we still limit the horizontal movement
-var wall_jump_lock: float = 0.0
-# Wall jump & slide
-const WALL_JUMP_LOCK_TIME: float = 0.05
-
-var is_sleeping: bool = true
-var has_woken_up: bool = false
-var is_wall_slide: bool = false
-var is_wall_climbing: bool = false
+var facing_direction = 1 # Looking right by default
+var wall_direction = 0
+var elapsed_time = 0.0
 
 func bounce(force: Vector2):
 	velocity = force
 
-func _physics_process(delta: float) -> void:
-	#for action_name in InputMap.get_actions():
-		#if Input.is_action_pressed(action_name):
-			##is_sleeping = false
-			#state = State.IDLE
-			#break
-			
+func _physics_process(delta: float) -> void:			
 	if not is_on_floor():
 		if velocity.y < 0:
 			# Apply down gravity
@@ -77,52 +51,42 @@ func _physics_process(delta: float) -> void:
 			velocity.y += FALL_GRAVITY * delta
 			if velocity.y > MAX_FALL_SPEED:
 				velocity.y = MAX_FALL_SPEED
-
-	# Handle jump
-	if Input.is_action_just_pressed("jump"):
-		if is_on_floor() || !coyote_timer.is_stopped():
-			velocity.y = JUMP_VELOCITY
 			
 	# Handle variable jump
 	if Input.is_action_just_released("jump") and velocity.y < 0:
 		velocity.y *= decelerate_on_jump_release
 	
-	# Handle sneak
-	if Input.is_action_pressed("sneak") && is_on_floor():
-		# Starts as crouch, then he sneaks as he walks
-		is_sneaking = true
-	else:
-		is_sneaking = false
-	
 	# Get the input direction and handle the movement/deceleration
 	# As good practice, you should replace UI actions with custom gameplay actions
 	var direction := Input.get_axis("move_left", "move_right")
 	
-	# 		if not coyote_timer.is_stopped():
-	
 	match state:
-		State.SLEEP:
-			if is_on_floor():
-				animated_stripe.play("sleep")
 		State.NORMAL:
-			if is_on_floor():
+			if is_on_floor() or !coyote_timer.is_stopped():
 				if Input.is_action_just_pressed("jump"):
 					state = State.JUMPING
 					velocity.y = JUMP_VELOCITY
 					animated_stripe.play("jump")
-				if direction:
-					# Move from current velocity to running, using delta
+					elapsed_time = 0
+				elif Input.is_action_pressed("sneak") && is_on_floor():
+					state = State.SNEAK
+					elapsed_time = 0
+				elif direction:
 					velocity.x = move_toward(velocity.x, direction * SPEED, SPEED * acceleration)
 					animated_stripe.flip_h = direction < 0
 					animated_stripe.play("run")
+					facing_direction = sign(direction)
+					elapsed_time = 0
 				else:
 					velocity.x = move_toward(velocity.x, 0, SPEED * deceleration)
-					# Only switch to idle once fully stopped
-					if abs(velocity.x) < 5:
-						velocity.x = 0
-						animated_stripe.play("idle")
+					if abs(velocity.x) == 0:
+						elapsed_time += delta
+						if elapsed_time >= 60:
+							animated_stripe.play("sleep")
+						else:
+							animated_stripe.play("idle")
 			else:
-				animated_stripe.play("fall")
+				state = State.JUMPING
 		State.JUMPING:
 			if direction != 0:
 				velocity.x = move_toward(velocity.x, direction * SPEED, SPEED * acceleration * 0.5)
@@ -131,14 +95,86 @@ func _physics_process(delta: float) -> void:
 				animated_stripe.play("jump")
 			else:
 				animated_stripe.play("fall")
+			if not is_on_floor() and is_on_wall() and velocity.y > 0 and velocity.x != 0:
+				# Landed on wall
+				facing_direction = sign(direction)
+				state = State.WALL_CLIMB
+			elif is_on_floor():
+				state = State.NORMAL
+		State.SNEAK:
+			if direction:
+				velocity.x = direction * SNEAK_SPEED
+				animated_stripe.flip_h = direction < 0
+				animated_stripe.play("sneak")
+			else:
+				velocity.x = 0
+				animated_stripe.play("crouch")
+				collision_shape.shape.radius = 30
+				collision_shape.position = Vector2(0.0, -4.0)
+			if Input.is_action_just_released("sneak"):
+				state = State.NORMAL
+				collision_shape.shape.radius = 60
+				collision_shape.position = Vector2(0.0, -7.0)
+		State.WALL_CLIMB:
+			# wall_direction is -1 for up, and +1 for down
+			var wall_direction = direction
+			if facing_direction > 0:
+				# Climbing a right wall, we need to change the sign of the direction, left wall is kept unchanged
+				wall_direction = -direction
+				velocity.y = wall_direction * WALL_CLIMB_SPEED
+			else:
+				velocity.y = wall_direction * WALL_CLIMB_SPEED
+			if wall_direction == 0:
+				animated_stripe.play("wall_crouch")
+			else:
+				if wall_direction > 0:
+					state = State.WALL_SLIDE
+				else:
+					animated_stripe.play("wall_climb")
+			if Input.is_action_just_pressed("jump"):
+				wall_jump()
+			if not is_on_wall():
+				animated_stripe.rotation_degrees = 0
+				state = State.NORMAL
+		State.WALL_SLIDE:
+			animated_stripe.play("wall_slide")
+			velocity.y = WALL_SLIDE_SPEED
+			if facing_direction > 0:
+				# Climbing a right wall
+				if Input.is_action_just_pressed("move_right"):
+					state = State.WALL_CLIMB
+				elif Input.is_action_pressed("move_left"):
+					# Make descend faster
+					velocity.y *= 2
+			else:
+				# Climbing a left wall
+				if Input.is_action_just_pressed("move_left"):
+					state = State.WALL_CLIMB
+				elif Input.is_action_pressed("move_right"):
+					# Make descend faster
+					velocity.y *= 2
+			if Input.is_action_just_pressed("jump"):
+				wall_jump()
 			if is_on_floor():
 				state = State.NORMAL
-				
+	
+	var was_on_floor = is_on_floor() 
+	
 	# This updates collision and floor detection, resets velocity, etc (to use methods like is_on_floor, etc)
 	move_and_slide()	
+	
+	if was_on_floor && not is_on_floor():
+		coyote_timer.start()
+		
+func wall_jump():
+	state = State.NORMAL
+	velocity.y = JUMP_VELOCITY
+	velocity.x = -facing_direction * WALL_JUMP_PUSH_FORCE
+	animated_stripe.flip_h = true if facing_direction > 0 else false
 
 func _on_animated_sprite_2d_animation_finished() -> void:
-	if animated_stripe.animation == "land":
-		print("Finished landed animation")
-		animated_stripe.play("idle")
-		just_landed = false
+	pass
+	#if animated_stripe.animation == "land":
+		#print("Finished landed animation")
+		#animated_stripe.play("idle")
+		#just_landed = false
